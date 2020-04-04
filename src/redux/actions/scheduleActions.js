26ad4f0427c5bcabd062365cd4dedf911d49d555
muhjamaludin <muhammad1400ft2015@student.uny.ac.id>
2020-04-04 13:13:44 +0700

    update getall for actions, route, and schedule

---
+++ src/redux/actions/scheduleActions.js
@@ -0,0 +1,18 @@
+import Config from '../../utils/config'
+import axios from 'axios'
+import qs from 'querystring'
+axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem(
+  'token'
+)}`
+
+export const getAgents = () => async (dispatch) => {
+  try {
+    const res = await axios.get(Config.APP_BACKEND.concat('schedule'))
+    dispatch({
+      type: 'GET_SCHEDULES',
+      payload: res.data,
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
