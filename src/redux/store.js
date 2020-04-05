f96bde4700148fb3fc4770a8b24ab38f68b9c4a0
muhjamaludin <muhammad1400ft2015@student.uny.ac.id>
2020-04-05 18:50:47 +0700

    add devtools-extension

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,32 +1,33 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import promiseMidlleware from 'redux-promise-middleware';
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 // import logger, { createLogger } from 'redux-logger'
-import storage from 'redux-persist/lib/storage';
-import { persistStore, persistReducer } from 'redux-persist';
-import hardSet from 'redux-persist/lib/stateReconciler/hardSet';
-import reducer from './reducer/reducer';
+import storage from 'redux-persist/lib/storage'
+import { persistStore, persistReducer } from 'redux-persist'
+import hardSet from 'redux-persist/lib/stateReconciler/hardSet'
+import reducer from './reducer/reducer'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 const config = {
   key: 'root',
   storage,
   stateReconciler: hardSet,
-  timeout: null
-};
+  timeout: null,
+}
 
-const persisteReducer = persistReducer(config, reducer);
+const persisteReducer = persistReducer(config, reducer)
 // const loggerMiddleware = createLogger()
 
-export default function() {
+export default function () {
   const store = createStore(
     persisteReducer,
-    applyMiddleware(
-      promiseMidlleware,
-      // logger,
-      // loggerMiddleware,
-      thunk
+    composeWithDevTools(
+      applyMiddleware(
+        // logger,
+        // loggerMiddleware,
+        thunk
+      )
     )
-  );
-  const persistor = persistStore(store);
-  return { store, persistor };
+  )
+  const persistor = persistStore(store)
+  return { store, persistor }
 }
